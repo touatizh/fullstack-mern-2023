b7f939aaa4f3d2b3eca946309783b53df91d43cb
Helmi Touati <touatizh@gmail.com>
2023-10-26 16:39:20 +0100

    Assignment(Predict Destructuring): Initial upload

---
+++ javascript/fundamentals/predict_destructuring.js
@@ -0,0 +1,20 @@
+// Problem 1 output:
+//*     Tesla
+//*     Mercedes
+
+// Problem 2 output:
+//*     error undefined object name
+
+// Problem 3 output:
+//*     12345
+//*     undefined
+
+// Problem 4 output:
+//*     false
+//*     true
+
+// Problem 5 output:
+//*     value
+//*     [1,5,1,8,3,3]
+//*     1
+//*     5

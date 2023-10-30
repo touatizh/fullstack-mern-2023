619940c0143c7ad3f2484b89ef986a4612ea3e28
Helmi Touati <touatizh@gmail.com>
2023-10-30 14:06:10 +0100

    Assignment(Putting it Together)
    - Added a state variable to hold the age of the person
    - Added a button that increases the person's age by 1 year each time it is clicked
    - Age is now displayed as the current value of age from state and not the original value from the props object

--- React/Functional Components/prop_it_up/src/components/person_card.js
+++ React/Functional Components/prop_it_up/src/components/person_card.js
@@ -1,12 +1,20 @@
 import '../App.css';
 import './person_card.css'
+import { useState } from 'react'
+
 const PersonCard = (props) => {
-    const { firstName, lastName, age, hairColor } = props
+    const { firstName, lastName, hairColor } = props
+
+    const [age, setAge] = useState(props.age);
+
+    const incrementAge = () => setAge(age + 1);
+
     return (
         <div className="card">
             <h3>{lastName}, {firstName}</h3>
             <p>Age: {age}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={incrementAge}>Birthday button for {firstName} {lastName}</button>
         </div>
     );
 }

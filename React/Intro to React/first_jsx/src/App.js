76c169eda40c42cdca8e9cb27cd9bfbabc7d0729
Helmi Touati <touatizh@gmail.com>
2023-10-30 12:09:36 +0100

    Assignment(First JSX): Initial upload

---
+++ React/Intro to React/first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Dojo!</h1>
+      <div>
+        <h3>Things I need to do:</h3>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;

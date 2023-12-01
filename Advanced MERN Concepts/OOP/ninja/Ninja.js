603930e50783b464f1d258c78e0a1d23c28f1865
Helmi Touati <touatizh@gmail.com>
2023-12-01 21:00:38 +0100

    Assignment(Ninja)
    - Created a `Ninja` class with various attributes such as name, health, strength, and speed
    - Added a method for logging the Ninja's name to the console (`sayName`)
    - Added a method for displaying the Ninja's name, strength, speed, and health (`showStats`)
    - Added a method that allows the Ninja to drink sake to increase health by +10, with a maximum limit of 100 (`drinkSake`)
    
    Note: With this assignment I started practicing JSDoc Google pattern to provide a clear and concise comments for the class, constructor, and methods in order to enhance code readability and understanding.

---
+++ Advanced MERN Concepts/OOP/ninja/Ninja.js
@@ -0,0 +1,69 @@
+/**
+ * Represents a Ninja with name, health, strength, and speed attributes.
+ * @class
+ */
+class Ninja {
+    /**
+     * Creates a new Ninja.
+     * @constructor
+     * @param {string} name - The name of the Ninja.
+     * @param {number} [health=100] - The health of the Ninja (default is 100).
+     * @param {number} [speed=3] - The speed of the Ninja (default is 3).
+     * @param {number} [strength=3] - The strength of the Ninja (default is 3).
+     */
+    constructor(name, health = 100, speed = 3, strength = 3) {
+        /**
+         * The name of the Ninja.
+         * @type {string}
+         */
+        this.name = name;
+
+        /**
+         * The health of the Ninja.
+         * @type {number}
+         */
+        this.health = health;
+
+        /**
+         * The strength of the Ninja.
+         * @type {number}
+         */
+        this.strength = strength;
+
+        /**
+         * The speed of the Ninja.
+         * @type {number}
+         */
+        this.speed = speed;
+    }
+
+
+    /**
+     * Logs the Ninja's name to the console.
+     * @method
+     */
+    sayName() {
+        console.log(this.name);
+    }
+
+    /**
+     * Logs the Ninja's name, strength, speed, and health to the console.
+     * @method
+     */
+    showStats() {
+        console.log(
+            `Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`
+        );
+    }
+
+    /**
+     * Adds +10 health to the ninja, up to a maximum of 100.
+     * @method
+     * @returns {Ninja} The current Ninja instance for method chaining.
+     */
+    drinkSake() {
+        const healthIncrease = 10
+        this.health = (this.health + healthIncrease) >= 100 ? 100 : this.health + healthIncrease;
+        return this;
+    }
+}
